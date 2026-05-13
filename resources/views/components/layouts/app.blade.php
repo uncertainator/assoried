8d48ed4d1535b80919de26295d2b35f0b213894d
uncertainator <jveraguth@gmail.com>
2026-05-13 23:00:44 +0200

    Bugfixes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -23,8 +23,10 @@
                 <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" aria-hidden="true"><polyline points="6 9 12 15 18 9"/></svg>
             </span>
             <div class="fb-nav-dropdown-menu">
-                <a href="{{ route('lab.external.citoyen') }}" class="{{ request()->routeIs('lab.external.citoyen*') ? 'active' : '' }}">Particuliers</a>
-                <a href="{{ route('lab.external.entreprise') }}" class="{{ request()->routeIs('lab.external.entreprise*') ? 'active' : '' }}">Entreprises</a>
+                <div class="fb-nav-dropdown-inner">
+                    <a href="{{ route('lab.external.citoyen') }}" class="{{ request()->routeIs('lab.external.citoyen*') ? 'active' : '' }}">Particuliers</a>
+                    <a href="{{ route('lab.external.entreprise') }}" class="{{ request()->routeIs('lab.external.entreprise*') ? 'active' : '' }}">Entreprises</a>
+                </div>
             </div>
         </div>
         @auth
